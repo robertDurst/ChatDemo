a608349e6b8cc819f7cc0cda68345a1bf0b9f5b2
robertdurst <rsdurst@colby.edu>
2018-08-19 23:56:32 -0700

    Miller-Rabin, is prime, and clone/clippy cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-extern crate wasm_bindgen;
 extern crate num;
+extern crate num_bigint as biggint;
 extern crate num_traits;
+extern crate rand;
+extern crate wasm_bindgen;
 
 pub mod crypto_math;
 
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -15,5 +17,3 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Why hello there, {}!", name));
 }
-
-
