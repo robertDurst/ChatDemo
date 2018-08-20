f743218406a63e629ea16c8492b6d9acd55567a5
robertdurst <rsdurst@colby.edu>
2018-08-19 23:56:32 -0700

    Miller-Rabin, is prime, and clone/clippy cleanup

--- src/crypto_math.rs
+++ src/crypto_math.rs
@@ -1,6 +1,20 @@
-use num::bigint::BigInt;
-use num_traits::{Zero, One};
-use wasm_bindgen::prelude::*;
+use num::bigint::{BigInt, RandBigInt, ToBigInt};
+use num_traits::{One, Zero};
+use rand;
+
+static SMALL_PRIMES: &'static [i32] = &[
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+    197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
+    311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421,
+    431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547,
+    557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+    661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+    809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929,
+    937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+];
+
+static BASES: &'static [i32] = &[2, 3, 5, 7, 11];
 
 macro_rules! StringToNumber {
     ($e: expr) => {
@@ -98,10 +112,11 @@ pub fn gcd(a: &str, b: &str) -> String {
     let mut y = StringToNumber!(b);
 
     while y != Zero::zero() {
-        let r = x % y.clone();
+        let r = x % &y;
         x = y;
         y = r;
     }
+
     return NumberToString!(x);
 }
 
@@ -199,10 +214,9 @@ pub fn extended_gcd(a: &str, b: &str) -> (String, String) {
         let temp_t = t.clone();
         t = old_t - &q * t;
         old_t = temp_t;
-
     }
 
-    return (NumberToString!(old_t), NumberToString!(old_s));
+    (NumberToString!(old_t), NumberToString!(old_s))
 }
 
 #[cfg(test)]
@@ -214,7 +228,7 @@ mod test_extended_gcd {
         let a = "12";
         let b = "17";
         let expected_a = "-7".to_string();
-        let expected_b= "5".to_string();
+        let expected_b = "5".to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 
@@ -223,7 +237,7 @@ mod test_extended_gcd {
         let a = "180";
         let b = "150";
         let expected_a = "1".to_string();
-        let expected_b= "-1".to_string();
+        let expected_b = "-1".to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 
@@ -232,7 +246,7 @@ mod test_extended_gcd {
         let a = "39392";
         let b = "9372";
         let expected_a = "950".to_string();
-        let expected_b="-3993".to_string();
+        let expected_b = "-3993".to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 
@@ -241,7 +255,7 @@ mod test_extended_gcd {
         let a = "29837362344";
         let b = "20938934792";
         let expected_a = "70028498".to_string();
-        let expected_b= "-99788537".to_string();
+        let expected_b = "-99788537".to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 
@@ -250,7 +264,7 @@ mod test_extended_gcd {
         let a = "298373623442234243";
         let b = "224224424293284938";
         let expected_a = "29778059398942417".to_string();
-        let expected_b= "-39625422207881285".to_string();
+        let expected_b = "-39625422207881285".to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 
@@ -258,20 +272,24 @@ mod test_extended_gcd {
     fn x_large() {
         let a = "1873817317893712873298173982173982173897128738912738217371897381374891378943789";
         let b = "9188937128738173912371837981739817238917246812647812678394619836281693618963297";
-        let expected_a = "-1486080468736810267748473400213603987572344688308283414544810257982300340964938".to_string();
-        let expected_b= "303043026531734365807887908508346161442903254640489390676789939813131430349539".to_string();
+        let expected_a =
+            "-1486080468736810267748473400213603987572344688308283414544810257982300340964938"
+                .to_string();
+        let expected_b =
+            "303043026531734365807887908508346161442903254640489390676789939813131430349539"
+                .to_string();
         assert_eq!(extended_gcd(a, b), (expected_a, expected_b));
     }
 }
 
 // Ported from: http://www.maths.dk/teaching/courses/math398-spring2017/code/cryptomath.txt
 pub fn mod_inverse(a: &str, m: &str) -> Option<String> {
-    let gcd_num = StringToNumber!(gcd(a.clone(),m.clone()));
+    let gcd_num = StringToNumber!(gcd(&a, &m));
     if gcd_num != One::one() {
-        return None
+        return None;
     }
 
-    let (u, _) = extended_gcd(a.clone(), m.clone());
+    let (u, _) = extended_gcd(&a, &m);
     let u_num = StringToNumber!(u);
     let m_num = StringToNumber!(m);
     Some(NumberToString!(u_num % m_num))
@@ -321,11 +339,194 @@ mod test_mod_inverse {
         assert_eq!(mod_inverse(a, m), expected);
     }
 
-      #[test]
+    #[test]
     fn x_large() {
         let a = "1873817317893712873298173982173982173897128738912738217371897381374891378";
         let m = "9188937128738173912371837981739817238917246812647812678394619836281693618963297";
-        let expected = Some("-996417904483222556354083958060155179719360472118047976841259037232297184027911".to_string());
+        let expected = Some(
+            "-996417904483222556354083958060155179719360472118047976841259037232297184027911"
+                .to_string(),
+        );
         assert_eq!(mod_inverse(a, m), expected);
+    }
+}
+
+// Check out: https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test
+pub fn miller_rabin(n: &str) -> bool {
+    let zero: BigInt = StringToNumber!("0");
+    let one: BigInt = StringToNumber!("1");
+    let two: BigInt = StringToNumber!("2");
+
+    let n_num: BigInt = StringToNumber!(n);
+    let n_minus_one = &n_num - &one;
+
+    if n_num == two {
+        return true;
+    }
+
+    if n_num < two || &n_num % &two == zero {
+        return false;
+    }
+
+    let mut s: BigInt = zero.clone();
+    let mut d: BigInt = &n_num - &one;
+
+    while &d % &two == zero {
+        s += &one;
+        d /= &two;
+    }
+
+    // 50 here is a parameter for accuracy
+    for _ in 0..50 {
+        let mut rng = rand::thread_rng();
+        let a_num = rng.gen_bigint_range(&two, &n_minus_one);
+        let a_str = NumberToString!(&a_num);
+
+        let gcd_str = gcd(&a_str, &n);
+        let gcd_num = StringToNumber!(gcd_str);
+
+        if gcd_num != one {
+            return false;
+        }
+
+        let mut x_num = a_num.modpow(&d, &n_num);
+
+        if x_num == one || x_num == n_minus_one {
+            continue;
+        }
+
+        let mut is_witness = true;
+        let mut r = one.clone();
+
+        while r < s && is_witness {
+            x_num = x_num.modpow(&two, &n_num);
+
+            if x_num == n_minus_one {
+                is_witness = false;
+            }
+
+            r += &one;
+        }
+
+        if is_witness {
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn is_prime(n: &str) -> bool {
+    let zero = StringToNumber!("0");
+    let one = StringToNumber!("1");
+    let two = StringToNumber!("2");
+
+    let n_num = StringToNumber!(n);
+
+    let n_minus_one = &n_num - &one;
+
+    if n_num < two {
+        return false;
+    }
+
+    let small_primes_as_bigints: Vec<BigInt> = SMALL_PRIMES
+        .iter()
+        .map(|x| x.to_bigint().unwrap())
+        .collect();
+    let is_small_prime = small_primes_as_bigints.contains(&n_num);
+
+    if is_small_prime {
+        return true;
+    }
+
+    for prime in small_primes_as_bigints {
+        if &n_num % &prime == zero {
+            return false;
+        }
+    }
+
+    let bases_as_bigints: Vec<BigInt> = BASES.iter().map(|x| x.to_bigint().unwrap()).collect();
+
+    for base in &bases_as_bigints {
+        if base.modpow(&n_minus_one, &n_num) != StringToNumber!("1") {
+            return false;
+        }
+    }
+
+    miller_rabin(n)
+}
+
+#[cfg(test)]
+mod test_is_prime_and_rabin_miller {
+    use super::*;
+
+    #[test]
+    fn miniscule_prime() {
+        let a = "3";
+        let expected = true;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn miniscule_not_prime() {
+        let a = "4";
+        let expected = false;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn tiny_prime() {
+        let a = "1049";
+        let expected = true;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn tiny_not_prime() {
+        let a = "1050";
+        let expected = false;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn small_prime() {
+        let a = "100103";
+        let expected = true;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn small_not_prime() {
+        let a = "100105";
+        let expected = false;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn medium_prime() {
+        let a = "100000015333";
+        let expected = true;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn medium_not_prime() {
+        let a = "100000015334";
+        let expected = false;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn large_prime() {
+        let a = "335184372088831";
+        let expected = true;
+        assert_eq!(is_prime(a), expected);
+    }
+
+    #[test]
+    fn large_not_prime() {
+        let a = "335184372088832";
+        let expected = false;
+        assert_eq!(is_prime(a), expected);
     }
 }
