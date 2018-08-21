4d710790040e83b4136a4c0ec21e6574cf942703
robertdurst <rsdurst@colby.edu>
2018-08-20 18:54:53 -0700

    Keypair first try, add keypair to chat app, and fix travis

--- crypto_module.d.ts
+++ crypto_module.d.ts
@@ -1,3 +1,17 @@
 /* tslint:disable */
+export function generate_prime(arg0: number, arg1: number, arg2: Uint8Array): string;
+
 export function greet(arg0: string): void;
 
+export class Keypair {
+free(): void;
+static  new(arg0: Uint8Array): Keypair;
+
+ public_key_display_wasm(): string;
+
+}
+export class Keydouble {
+free(): void;
+static  new(arg0: string, arg1: string, arg2: boolean): Keydouble;
+
+}
--- crypto_module.js
+++ crypto_module.js
@@ -1,12 +1,6 @@
 /* tslint:disable */
 import * as wasm from './crypto_module_bg';
 
-const TextDecoder = typeof self === 'object' && self.TextDecoder
-    ? self.TextDecoder
-    : require('util').TextDecoder;
-
-let cachedDecoder = new TextDecoder('utf-8');
-
 let cachegetUint8Memory = null;
 function getUint8Memory() {
     if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
@@ -15,13 +9,55 @@ function getUint8Memory() {
     return cachegetUint8Memory;
 }
 
+function passArray8ToWasm(arg) {
+    const ptr = wasm.__wbindgen_malloc(arg.length * 1);
+    getUint8Memory().set(arg, ptr / 1);
+    return [ptr, arg.length];
+}
+
+const TextDecoder = typeof self === 'object' && self.TextDecoder
+    ? self.TextDecoder
+    : require('util').TextDecoder;
+
+let cachedDecoder = new TextDecoder('utf-8');
+
 function getStringFromWasm(ptr, len) {
     return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
-export function __wbg_alert_203900472737c5ba(arg0, arg1) {
-    let varg0 = getStringFromWasm(arg0, arg1);
-    alert(varg0);
+let cachedGlobalArgumentPtr = null;
+function globalArgumentPtr() {
+    if (cachedGlobalArgumentPtr === null) {
+        cachedGlobalArgumentPtr = wasm.__wbindgen_global_argument_ptr();
+    }
+    return cachedGlobalArgumentPtr;
+}
+
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null || cachegetUint32Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory;
+}
+/**
+* @param {number} arg0
+* @param {number} arg1
+* @param {Uint8Array} arg2
+* @returns {string}
+*/
+export function generate_prime(arg0, arg1, arg2) {
+    const [ptr2, len2] = passArray8ToWasm(arg2);
+    const retptr = globalArgumentPtr();
+    wasm.generate_prime(retptr, arg0, arg1, ptr2, len2);
+    const mem = getUint32Memory();
+    const rustptr = mem[retptr / 4];
+    const rustlen = mem[retptr / 4 + 1];
+    if (rustptr === 0) return;
+    const realRet = getStringFromWasm(rustptr, rustlen).slice();
+    wasm.__wbindgen_free(rustptr, rustlen * 1);
+    return realRet;
+    
 }
 
 const TextEncoder = typeof self === 'object' && self.TextEncoder
@@ -37,6 +73,11 @@ function passStringToWasm(arg) {
     getUint8Memory().set(buf, ptr);
     return [ptr, buf.length];
 }
+
+export function __wbg_alert_203900472737c5ba(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+    alert(varg0);
+}
 /**
 * @param {string} arg0
 * @returns {void}
@@ -51,6 +92,80 @@ export function greet(arg0) {
         
     }
     
+}
+
+/**
+*/
+export class Keypair {
+    
+    static __construct(ptr) {
+        return new Keypair(ptr);
+    }
+    
+    constructor(ptr) {
+        this.ptr = ptr;
+    }
+    
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+        wasm.__wbg_keypair_free(ptr);
+    }
+    /**
+    * @param {Uint8Array} arg0
+    * @returns {Keypair}
+    */
+    static new(arg0) {
+        const [ptr0, len0] = passArray8ToWasm(arg0);
+        return Keypair.__construct(wasm.keypair_new(ptr0, len0));
+    }
+    /**
+    * @returns {string}
+    */
+    public_key_display_wasm() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        const retptr = globalArgumentPtr();
+        wasm.keypair_public_key_display_wasm(retptr, this.ptr);
+        const mem = getUint32Memory();
+        const rustptr = mem[retptr / 4];
+        const rustlen = mem[retptr / 4 + 1];
+        
+        const realRet = getStringFromWasm(rustptr, rustlen).slice();
+        wasm.__wbindgen_free(rustptr, rustlen * 1);
+        return realRet;
+        
+    }
+}
+/**
+*/
+export class Keydouble {
+    
+    static __construct(ptr) {
+        return new Keydouble(ptr);
+    }
+    
+    constructor(ptr) {
+        this.ptr = ptr;
+    }
+    
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+        wasm.__wbg_keydouble_free(ptr);
+    }
+    /**
+    * @param {string} arg0
+    * @param {string} arg1
+    * @param {boolean} arg2
+    * @returns {Keydouble}
+    */
+    static new(arg0, arg1, arg2) {
+        const [ptr0, len0] = passStringToWasm(arg0);
+        const [ptr1, len1] = passStringToWasm(arg1);
+        return Keydouble.__construct(wasm.keydouble_new(ptr0, len0, ptr1, len1, arg2 ? 1 : 0));
+    }
 }
 
 export function __wbindgen_throw(ptr, len) {
