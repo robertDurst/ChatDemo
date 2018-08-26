8177a6648a64aaa5789a6a91dcac598f5c507fdf
robertdurst <rsdurst@colby.edu>
2018-08-25 22:55:17 -0700

    decouple rsa and crypto math

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ extern crate wasm_bindgen;
 #[macro_use]
 extern crate lazy_static;
 
-/// Currently this module contains basic crypto math primitives (GCD, modular inverse, etc.) and
-/// RSA encryption/decryption.
+/// The crypto_math module contains basic crypto math primitives (GCD, modular inverse, etc.).
 pub mod crypto_math;
+
+/// The rsa module contains methods for encryption/decryption of messages utilizng RSA asymmetric,
+/// public key cryptosystem.
+pub mod rsa;
