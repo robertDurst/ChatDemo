c0f91b6146e289d7557c324707b32218eb7aa1ba
robertdurst <rsdurst@colby.edu>
2018-08-25 21:37:28 -0700

    Very basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! Crypto Math is a crate containing basic cryptographic primitives and encryption schemes.
+
 extern crate num;
 extern crate num_traits;
 extern crate rand;
@@ -6,4 +8,6 @@ extern crate wasm_bindgen;
 #[macro_use]
 extern crate lazy_static;
 
+/// Currently this module contains basic crypto math primitives (GCD, modular inverse, etc.) and
+/// RSA encryption/decryption.
 pub mod crypto_math;
